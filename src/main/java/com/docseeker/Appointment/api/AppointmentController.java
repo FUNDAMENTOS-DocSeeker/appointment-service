9a57474600b6a13852782f2eb46d2961677bfbc0
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-09-26 13:29:57 -0500

    feat: change api endpoint

--- src/main/java/com/docseeker/Appointment/api/AppointmentController.java
+++ src/main/java/com/docseeker/Appointment/api/AppointmentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/appointment")
+@RequestMapping("api/v1/appointment")
 @AllArgsConstructor
 public class AppointmentController {
 
