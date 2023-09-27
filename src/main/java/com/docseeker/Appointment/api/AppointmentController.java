32fdd3df65e6b93e901476388db63aa30bb989a3
Gsak79 <gonchissn@gmail.com>
2023-09-27 09:07:36 -0500

    fix: add plural to endpoint

--- src/main/java/com/docseeker/Appointment/api/AppointmentController.java
+++ src/main/java/com/docseeker/Appointment/api/AppointmentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/appointment")
+@RequestMapping("api/v1/appointments")
 @AllArgsConstructor
 public class AppointmentController {
 
